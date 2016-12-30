df962cc44782c8c34e8c8de37ff7dc517a001dde
Peter Shackelford <peter.shackelford@gmail.com>
2016-12-30 18:34:54 +0000

    first commit

---
+++ cider.php
@@ -0,0 +1,252 @@
+<?php
+/**
+ * Plugin Name:     Cider
+ * Plugin URI:      http://twitter.com/pixelplow
+ * Description:     Extensable Citation Plugin
+ * Author:          Peter Shackelford
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     cider
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cider
+ */
+
+if ( file_exists( plugin_dir_path( __FILE__ ) . '/inc/simplehtmldom/simple_html_dom.php' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . '/inc/simplehtmldom/simple_html_dom.php' );
+}
+if ( file_exists( plugin_dir_path( __FILE__ ) . '/meta-boxes/cider-meta.php' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . '/meta-boxes/cider-meta.php' );
+}
+
+class ExternalMetaObject {
+	//Values needed to create object
+	public $_html;
+	public $url;
+	public $source_xpath;
+
+	// Object Properties
+	public $json_ld_meta;
+	public $schema_meta;
+	public $opem_graph_meta;
+	public $twitter_meta;
+	public $custom_meta;
+
+	public function __construct( $url ) {
+		$this->url = $url;
+		$this->set_default_values();
+		$this->create_source_xpath();
+		$this->get_json_ld_meta();
+		$this->get_open_graph_meta();
+		$this->get_twitter_meta();
+		$this->get_jstor_meta();
+	}
+
+	public function set_default_values() {
+		$url         = rtrim( $this->url, "/" );
+		$response    = wp_remote_get( $url );
+		$body        = wp_remote_retrieve_body( $response );
+		$html        = $body;
+		$this->_html = $html;
+	}
+
+	public function get_best_meta_data() {
+		if ( ! empty( $this->json_ld_meta ['cider_title'] ) ) {
+			$cider_meta = $this->json_ld_meta;
+		} elseif ( ! empty( $this->opem_graph_meta ['cider_title'] ) ) {
+			$cider_meta = $this->opem_graph_meta;
+		} elseif ( ! empty( $this->twitter_meta ['cider_title'] ) ) {
+			$cider_meta = $this->twitter_meta;
+		}
+		if(! empty ($this->custom_meta ['cider_title'])){
+			$cider_meta = $this->custom_meta;
+		}
+		$larb = 'food';
+
+		return $cider_meta;
+	}
+
+	public function create_source_xpath() {
+		$dom = new DOMDocument();
+		libxml_use_internal_errors( TRUE );
+		@$dom->loadHTML( $this->_html );
+		libxml_use_internal_errors( FALSE );
+		$xpath              = new DOMXpath( $dom );
+		$this->source_xpath = $xpath;
+	}
+
+	public function get_json_ld_meta() {
+		$xpath       = $this->source_xpath;
+		$jsonScripts = $xpath->query( '//script[@type="application/ld+json"]' );
+		if ( empty( $jsonScripts->length ) ) {
+			return FALSE;
+		}
+		$json                            = trim( $jsonScripts->item( 0 )->nodeValue );
+		$data                            = json_decode( $json );
+		$larb = 'food';
+		$cider_meta['cider_title']       = trim( $data->headline );
+		$cider_meta['cider_contributor'] = trim( $data->author->name );
+		if ( empty( $cider_meta['cider_contributor'] ) ) {
+			$cider_meta['cider_contributor'] = trim( $data->creator[0] );
+		}
+		$cider_meta['cider_publication'] = trim( $data->publisher->name );
+		$cider_meta['cider_link']        = esc_url( $this->url );
+		$this->json_ld_meta = $cider_meta;
+	}
+
+	public function get_open_graph_meta() {
+		$html            = $this->_html;
+		$xpath           = $this->source_xpath;
+		$query           = '//*/meta[starts-with(@property, \'og:\')]';
+		$metas           = $xpath->query( $query );
+		$open_graph_meta = array();
+		foreach ( $metas as $meta ) {
+			$property                     = $meta->getAttribute( 'property' );
+			$content                      = $meta->getAttribute( 'content' );
+			$open_graph_meta[ $property ] = $content;
+		}
+		$open_graph_meta = array_filter( $open_graph_meta, function ( $og_meta ) {
+			$allowed = [
+				'og:site_name',
+				'og:title',
+				'og:url',
+				'article:author'
+			];
+
+			return in_array( $og_meta, $allowed );
+		},
+			ARRAY_FILTER_USE_KEY
+		);
+		$open_graph_meta['cider_title']       = $open_graph_meta['og:title'];
+		$open_graph_meta['cider_publication'] = $open_graph_meta['og:site_name'];
+		$open_graph_meta['cider_link']        = $open_graph_meta['og:url'];
+		unset( $open_graph_meta['og:title'] );
+		unset( $open_graph_meta['og:site_name'] );
+		unset( $open_graph_meta['og:url'] );
+		$this->opem_graph_meta = $open_graph_meta;
+	}
+
+	public function get_twitter_meta() {
+		$html         = $this->_html;
+		$xpath        = $this->source_xpath;
+		$query        = '//*/meta[starts-with(@property, \'twitter:\')]';
+		$metas        = $xpath->query( $query );
+		$twitter_meta = array();
+
+		foreach ( $metas as $meta ) {
+			$property                  = $meta->getAttribute( 'property' );
+			$content                   = $meta->getAttribute( 'content' );
+			$twitter_meta[ $property ] = $content;
+		}
+		$twitter_meta = array_filter( $twitter_meta, function ( $og_meta ) {
+			$allowed = [ 'twitter:site', 'twitter:title' ];
+
+			return in_array( $og_meta, $allowed );
+		},
+			ARRAY_FILTER_USE_KEY
+		);
+
+		$twitter_meta['cider_title']       = $twitter_meta['twitter:title'];
+		$twitter_meta['cider_publication'] = $twitter_meta['twitter:site'];
+		$twitter_meta['cider_link']        = esc_url( $this->url );;
+		unset( $twitter_meta['twitter:title'] );
+		unset( $twitter_meta['twitter:site'] );
+		$this->twitter_meta = $twitter_meta;
+	}
+
+	public function get_jstor_meta() {
+		if(!strpos( $this->url, 'jstor.org/stable' )!==false){
+			return;
+		}
+		$html                            = $this->_html;
+		$html = str_get_html($html);
+		$locators                        = array(
+			'h1',
+			'.contrib',
+			'.journal',
+			'.src',
+			'.publisher-link',
+			'.stable'
+		);
+		$larb ='food';
+		$cider_meta['cider_title']       = trim( $html->find( $locators[0], 0 )->plaintext );
+		$cider_meta['cider_contributor'] = trim( $html->find( $locators[1], 0 )->plaintext );
+		$cider_meta['cider_publication'] = trim( $html->find( $locators[2], 0 )->plaintext );
+		$cider_meta['cider_source']      = trim( $html->find( $locators[3], 0 )->plaintext );
+		$cider_meta['cider_publisher']   = trim( $html->find( $locators[4], 0 )->plaintext );
+		$cider_meta['cider_link']        = esc_url( $this->url );
+		$larb ='food';
+		$this->custom_meta               = $cider_meta;
+		$larb ='food';
+	}
+}
+
+class MetaUtilities {
+	public $cider_meta;
+
+	public function __construct() {
+		$this->cider_meta = [];
+	}
+
+	public function check_content_for_external_urls( $post_id ) {
+		$post     = get_post( $post_id );
+		$site_url = site_url();
+		$content  = $post->post_content;
+		$dom      = new DOMDocument();
+		@$dom->loadHTML( $post->post_content );
+		$xpath = new DOMXPath( $dom );
+		$hrefs = $xpath->evaluate( "/html/body//a" );
+		for ( $i = 0; $i < $hrefs->length; $i ++ ) {
+			$href   = $hrefs->item( $i );
+			$urls[] = $href->getAttribute( 'href' );
+		}
+		$urls = array_filter( $urls, function ( $url ) use ( $site_url ) {
+			if ( stripos( $url, $site_url ) === FALSE ) {
+				return TRUE; // true to keep url.
+			} else {
+				return FALSE; // false to filter it out.
+			}
+		} );
+
+		return $urls;
+	}
+
+	public function set_cider_meta_value( $value ) {
+		$this->cider_meta = array_merge( $this->cider_meta, $value );
+	}
+
+	public function get_cider_meta_value() {
+		return $this->cider_meta;
+	}
+}
+
+$obj  = new MetaUtilities;
+$post = get_post( 1 );
+$urls = $obj->check_content_for_external_urls( 1 );
+$larb = 'food';
+$external_item = new ExternalMetaObject( 'https://medium.com/@ericclemmons/javascript-fatigue-48d4011b6fc4#.a1yvowlk8' );
+//$external_item = new ExternalMetaObject( 'https://aeon.co/ideas/how-refugees-have-the-power-to-change-the-society-they-join' );
+//$test = $external_item->get_best_meta_data();
+$larb = 'food';
+//$external_item->set_default_values('www.jstor.org/stable/20020127');
+//$external_item->get_json_ld_meta();
+//$test = get_meta_tags('https://aeon.co/ideas/how-refugees-have-the-power-to-change-the-society-they-join');
+
+function populate_cider_meta( $post_id ) {
+	$larb       = 'food';
+	$obj        = new MetaUtilities;
+	$urls       = $obj->check_content_for_external_urls( $post_id );
+	$cider_meta = [ ];
+	array_filter( $urls, function ( $url ) use ( $post_id, $obj ) {
+		$source       = new ExternalMetaObject( $url );
+		$cider_meta[] = $source->get_best_meta_data();
+		$obj->set_cider_meta_value( $cider_meta );
+		$larb = 'food';
+	} );
+	delete_post_meta( $post_id, 'cider_repeat_group' );
+	$final_value = $obj->get_cider_meta_value();
+	update_post_meta( $post_id, 'cider_repeat_group', $final_value );
+}
+
+add_action( 'save_post', 'populate_cider_meta', 15, 2 );
+
---
+++ meta-boxes/cider-meta.php
@@ -0,0 +1,126 @@
+<?php
+if ( file_exists( plugin_dir_path( __FILE__ ) . '../inc/cmb2/init.php' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . '../inc/cmb2/init.php' );
+}
+add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ */
+function cmb2_sample_metaboxes() {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_cider_';
+
+	/**
+	 * Initiate the metabox
+	 */
+	$cmb = new_cmb2_box( array(
+		'id'           => $prefix . 'source_metabox',
+		'title'        => __( 'Sources Found', 'cmb2' ),
+		'object_types' => array( 'post', ), // Post type
+		'context'      => 'normal',
+		'priority'     => 'high',
+		'show_names'   => TRUE, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // Keep the metabox closed by default
+	) );
+
+	$group_field_id = $cmb->add_field( array(
+		'id'          => 'cider_repeat_group',
+		'type'        => 'group',
+		//'description' => __( 'Citations from content', 'cmb2' ),
+		'options'     => array(
+			'group_title' => __( 'Citation {#}', 'cmb2' ),
+			// since version 1.1.4, {#} gets replaced by row number
+			//'add_button'    => __( 'Add Another Entry', 'cmb2' ),
+			//'remove_button' => __( 'Remove Entry', 'cmb2' ),
+			'sortable'    => FALSE,
+			// beta
+			'closed'      => TRUE,
+			// true to have the groups closed by default
+		),
+		'after_group' => 'cider_add_js_for_repeatable_titles',
+	) );
+
+
+// Id's for group's fields only need to be unique for the group. Prefix is not needed.
+	$cmb->add_group_field( $group_field_id, array(
+		'name' => 'Title',
+		'id'   => 'cider_title',
+		'type' => 'text',
+	) );
+	$cmb->add_group_field( $group_field_id, array(
+		'name' => 'Author',
+		'id'   => 'cider_contributor',
+		'type' => 'text',
+	) );
+	$cmb->add_group_field( $group_field_id, array(
+		'name' => 'Publication Title',
+		'id'   => 'cider_publication',
+		'type' => 'text',
+	) );
+	$cmb->add_group_field( $group_field_id, array(
+		'name' => 'Source',
+		'id'   => 'cider_source',
+		'type' => 'text',
+	) );
+	$cmb->add_group_field( $group_field_id, array(
+		'name' => 'Publisher',
+		'id'   => 'cider_publisher',
+		'type' => 'text',
+	) );
+	$cmb->add_group_field( $group_field_id, array(
+		'name' => 'Link',
+		'id'   => 'cider_link',
+		'type' => 'text',
+	) );
+
+}
+
+function cider_add_js_for_repeatable_titles() {
+	add_action( 'admin_footer', 'cider_add_js_for_repeatable_titles_to_footer' );
+}
+
+// Populate field group label with contents of first field
+function cider_add_js_for_repeatable_titles_to_footer() {
+	?>
+	<script type="text/javascript">
+		jQuery(function ($) {
+			var $box = $(document.getElementById('cider_repeat_group_repeat'));
+			var replaceTitles = function () {
+				$box.find('.cmb-group-title').each(function () {
+					var $this = $(this);
+					var txt = $this.next().find('[id$="title"]').val();
+					var rowindex;
+					if (!txt) {
+						txt = $box.find('[data-grouptitle]').data('grouptitle');
+						if (txt) {
+							rowindex = $this.parents('[data-iterator]').data('iterator');
+							txt = txt.replace('{#}', ( rowindex + 1 ));
+						}
+					}
+					if (txt) {
+						$this.text(txt);
+					}
+				});
+			};
+			var replaceOnKeyUp = function (evt) {
+				var $this = $(evt.target);
+				var id = 'title';
+				if (evt.target.id.indexOf(id, evt.target.id.length - id.length) !== -1) {
+					console.log('val', $this.val());
+					$this.parents('.cmb-row.cmb-repeatable-grouping').find('.cmb-group-title').text($this.val());
+				}
+			};
+			$box
+				.on('cmb2_add_row cmb2_shift_rows_complete', function (evt) {
+					replaceTitles();
+				})
+				.on('keyup', replaceOnKeyUp);
+			replaceTitles();
+			// Hide the first generated empty item, add row and remove row
+			$('#cider_repeat_group_repeat').find('[data-iterator="0"],.cmb-add-row,button.cmb-remove-group-row').hide();
+		});
+	</script>
+	<?php
+}
